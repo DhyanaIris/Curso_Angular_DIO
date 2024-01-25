762a2da1c683a996ce2e924e4cfdea0aa7afd134
Dhyana Iris <dhyanairis@gmail.com>
2024-01-25 12:38:19 -0300

    estudando doCheck e onDestroy

--- angular/life-cycle/src/app/app.component.ts
+++ angular/life-cycle/src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { TitleComponent } from './title/title.component';
+import { CheckSampleComponent } from './check-sample/check-sample.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleComponent],
+  imports: [RouterOutlet, TitleComponent, CheckSampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'life-cycle';
+	isAliveCheckSample: boolean = true;
+
+	disposeChackSample(): void {
+		this.isAliveCheckSample = false;
+	}
 }
